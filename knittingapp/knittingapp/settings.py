18ac45dd409cd6820cd69e47b651593143f86f81
Imani Paul <imanipaul@imanis-mbp.lan>
2020-03-01 11:31:43 -0500

    Add auth rules to settings

--- knittingapp/knittingapp/settings.py
+++ knittingapp/knittingapp/settings.py
@@ -48,10 +48,15 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
-# REST_FRAMEWORK = {
-#     #use Django's standard 'django.contrib.auth' permissions,
-#     #or allow read-only access for unauthenticated users.
-# }
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework.authentication.TokenAuthentication',),
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
