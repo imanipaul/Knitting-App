bb3f6e46459fbf7daf7a2d7828964bb8471ac178
Imani Paul <imanipaul@imanis-mbp.lan>
2020-03-01 12:24:40 -0500

    Add all models

--- knittingapp/app/models.py
+++ knittingapp/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Yarn(models.Model): #one to one with project
+    weight = models.CharField(max_length=100)
+    skeins = models.IntegerField()
+
+class Needle(models.Model): #one to one with project (project has one needle, needle belongs to one project)
+    size = models.IntegerField()
+    length = models.IntegerField()
+    in_use = models.BooleanField(default=False)
+    circular = models.BooleanField(default=False)
+
+class Pattern(models.Model): #one to many with project (project has one pattern, but a pattern can have many projects)
+    url = models.URLField()
+    image = models.FileField(upload_to='uploads/', blank=True)
+
+class Note(models.Model):
+    content = models.TextField()
+
+class Project(models.Model):
+    name = models.CharField(max_length = 200)
+    row = models.IntegerField()
+    repeat = models.IntegerField()
+    pattern = models.ForeignKey(Pattern, on_delete=models.CASCADE)
+    yarn = models.OneToOneField(Yarn, on_delete=models.CASCADE)
+    note = models.OneToOneField(Note, on_delete=models.CASCADE)
--- knittingapp/knittingapp/settings.py
+++ knittingapp/knittingapp/settings.py
@@ -93,9 +93,13 @@ WSGI_APPLICATION = 'knittingapp.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'knitting_app',
+    'USER': 'postgres',
+    'PASSWORD': 'postgres',
+    'HOST': 'localhost',
+    'PORT': '5432',
     }
 }
 
