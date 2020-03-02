a1353289743077ab84b6efa54c387520e296a8c0
Imani Paul <imanipaul@imanis-mbp.lan>
2020-03-01 21:45:29 -0500

    Updates to requirements file

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/imanipaul/Desktop/Projects/django/Knitting-App/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
