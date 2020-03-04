8096fa6d2c56857f3b94621160677e942696839d
Imani Paul <imanipaul@imanis-mbp.lan>
2020-03-03 22:18:44 -0500

    Import dj database url package in settings

--- knittingapp/knittingapp/settings.py
+++ knittingapp/knittingapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '#02z$0l1&w8o(+kqi#_6-)*bp2qmvzdk3&!gpvimb#8cnd2&t7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
